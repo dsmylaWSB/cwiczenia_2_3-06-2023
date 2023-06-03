309f040e7791d42d3e7eee370eac6c8fdd877df4
dsmylaWSB <chx99935@student.chorzow.wsb.pl>
2023-06-03 17:37:37 +0200

    cw5

---
+++ cwiczenie_5/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Wprowadź dwie liczby a i b: ");
+
+Console.Write("a = ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("b = ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+
+if (a == b)
+{
+    Console.WriteLine("Obie liczby są takie same");
+}
+else
+{
+    Console.WriteLine($"Większa liczba to {Math.Max(a, b)}");
+}
