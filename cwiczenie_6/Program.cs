dbfeb73fdc922044562b4e8f33760cc59abf4fc5
dsmylaWSB <chx99935@student.chorzow.wsb.pl>
2023-06-03 18:01:50 +0200

    cw6

---
+++ cwiczenie_6/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Wprowadź długości boków trójkąta - a, b i c");
+
+Console.Write("a = ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("b = ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("c = ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+if( a == b && b == c)
+{
+    Console.WriteLine("Z tych boków powstanie trójkąt równoboczny");
+}
+else if( a == c || b == c || a == b)
+{
+    Console.WriteLine("Z tych boków powstanie trójkąt równoramienny, ale nie równoboczny");
+}
+else
+    Console.WriteLine("Z tych boków nie powstanie trójkąt równoboczny ani równoramienny");
+
+//wiem że wystarczyło zrobić tylko if i else z równobocznym, ale uznałem że takie ubarwienie nie zaszkodzi
