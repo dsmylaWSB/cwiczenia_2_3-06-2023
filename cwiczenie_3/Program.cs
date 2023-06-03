cdfd8c717a5be79c4df0f74357997021d194d377
dsmylaWSB <chx99935@student.chorzow.wsb.pl>
2023-06-03 16:05:56 +0200

    cw3

---
+++ cwiczenie_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Wprowadź swój wzrost i wagę: ");
+
+Console.Write("waga [kg] = ");
+double w = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("wzrost [m] = ");
+double h = Convert.ToDouble(Console.ReadLine());
+
+double BMI = w / (Math.Pow(h,2));
+Console.WriteLine($"Twoje BMI wynosi {BMI}");
