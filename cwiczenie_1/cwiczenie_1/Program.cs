03928262bef946bc64545f6d944609446e8d3d58
dsmylaWSB <chx99935@student.chorzow.wsb.pl>
2023-06-03 15:37:50 +0200

    cw1

---
+++ cwiczenie_1/cwiczenie_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Wprowadź trzy wartości a, b i c");
+
+Console.Write("a = ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("b = ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("c = ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+double srednia = (a + b + c) / 3;
+Console.WriteLine($"Średnia a, b i c wynosi {srednia}");
+
