96ec0fc9cf06d8397ea03e78dafddc4dfb8b00d3
dsmylaWSB <chx99935@student.chorzow.wsb.pl>
2023-06-10 02:54:02 +0200

    cw9

---
+++ cwiczenie_9/Program.cs
@@ -0,0 +1,24 @@
+﻿//Najłatwiej i najbardziej poprawnie byłoby użyć dodatkowej zmiennej tymczasowej, jednak polecenie tego zabrania
+
+Console.WriteLine("Podaj pierwszą liczbę: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Podaj drugą liczbę: ");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Podaj trzecią liczbę: ");
+double c = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Przed zamianą \n a= " + a + " b= " + b + " c= " + c);
+
+// Przechowujemy sumę wszystkich w a
+a = a + b + c;
+
+// Wartość a przenosimy do zmiennej b
+b = a - (b + c);
+
+// Wartość b przenosimy do zmiennej c
+c = a - (b + c);
+
+// Wartość c przenosimy do zmiennej a
+a = a - (b + c);
+
+Console.Write("Po zamianie \n a= " + a + " b= " + b + " c= " + c);
